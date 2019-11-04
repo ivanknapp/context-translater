812857d9764f8d0b20b48a866c0557e009b79eef
Ivan Knapp <ivanknapp@yandex.ru>
2019-11-04 17:49:02 +0300

    Initial Commit

---
+++ context-translater/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  context-translater
+//
+//  Created by Иван Кнапп on 04/11/2019.
+//  Copyright © 2019 Иван Кнапп. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ context-translater/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  context-translater
+//
+//  Created by Иван Кнапп on 04/11/2019.
+//  Copyright © 2019 Иван Кнапп. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
